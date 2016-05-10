1559c7c66d260ace322daafddc0f07271fc1dcea
AbdullahRaza <abdullah.raza17@gmail.com>
2016-05-10 21:26:50 +0500

    commit by abdullah

--- app/src/main/java/com/example/mrazawahga/git/MainActivity.java
+++ app/src/main/java/com/example/mrazawahga/git/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.mrazawahga.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
